03a02c673fa12ce3caf357dcdf14be928aea397d
Govind Dixit <32304546+GOVINDDIXIT@users.noreply.github.com>
2018-12-31 12:49:49 +0530

    Initial commit

---
+++ app/src/main/java/com/govinddixit/memeify/Constants.kt
@@ -0,0 +1,8 @@
+package com.govinddixit.memeify
+
+const val IMAGE_URI_KEY = "IMAGE_URI"
+const val BITMAP_WIDTH = "BITMAP_WIDTH"
+const val BITMAP_HEIGHT = "BITMAP_HEIGHT"
+
+
+//Intent extras constants
